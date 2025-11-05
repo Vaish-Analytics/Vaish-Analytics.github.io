aba813590ed39b48ee7a6b6eca0090b3ff7eb244
Vaish-Analytics <139734980+Vaish-Analytics@users.noreply.github.com>
2025-11-05 14:06:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let scrollBtn = document.querySelector(".scroll-button a");
 console.log(scrollBtn);
 let val;
 window.onscroll = function() {
-  if(document.documentElement.scrollTop > 20){
+  if(document.documentElement.scrollTop > 0){ //20
     nav.classList.add("sticky");
     scrollBtn.style.display = "block";
   }else{
